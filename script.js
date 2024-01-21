23cf785d28a474bdd6d640da80716e7a395c457f
Alexander Klein <agklein04@outlook.com>
2024-01-20 21:28:56 -0500

    Main UI Done, Need Mobile UI, Need Research Info

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+function downloadResume() {
+    // URL of the PDF file
+    const pdfUrl = 'assets/resume.pdf';
+    
+    // Create a temporary <a> element to trigger the download
+    const downloadLink = document.createElement('a');
+    downloadLink.href = pdfUrl;
+    downloadLink.download = 'iaconis_sophia_resume.pdf'; // Specify the filename
+    
+    // Simulate a click on the link to trigger the download
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    
+    // Clean up resources after the download link is clicked
+    document.body.removeChild(downloadLink);
+}
+
+function openEmail() {
+    window.open("mailto:sophia.iaconis@ufl.edu");
+}
